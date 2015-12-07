4d4d032740b4527dd2b761f15bb912127f48848e
Maurizio <maurizio.prazzoli@gmail.com>
2015-12-07 14:25:10 +0100

    Adding DisposableObject

---
+++ DisposableObject/Program.cs
@@ -0,0 +1,40 @@
+﻿
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        // Used to give objects different names.
+        private int ObjectNumber = 1;
+
+        // Create an object and do not dispose of it.
+        private void createButton_Click()
+        {
+            // Make an object.
+            DisposableClass obj = new DisposableClass();
+            obj.Name = "Create " + ObjectNumber.ToString();
+            ObjectNumber++;
+        }
+
+        // Create an object and dispose of it.
+        private void createAndDisposeButton_Click()
+        {
+            // Make an object.
+            DisposableClass obj = new DisposableClass();
+            obj.Name = "CreateAndDispose " + ObjectNumber.ToString();
+            ObjectNumber++;
+            // Dispose of the object.
+            obj.Dispose();
+        }
+
+
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            p.createAndDisposeButton_Click();
+            p.createButton_Click();
+            p.createButton_Click();
+            p.createAndDisposeButton_Click();
+        }
+
+    }
+}
